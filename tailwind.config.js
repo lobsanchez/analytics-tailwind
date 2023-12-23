fbec84d7ce2fab47c1cd091fea01c066ac83769e
Luis Óscar Bueno Sánchez <lobsanchez@hotmail.com>
2023-12-23 10:27:51 +0100

    Nuevo repositorio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./*.{html,php}",
+    "./assets/templates/*.{html,php}"
+  ],
   theme: {
     extend: {
       maxWidth: {
